ab36999b79f7f26a013e29ebdfa693df3313972c
Ryan Rasmussen <ryan.rasmussen@annelewisllc.com>
2019-07-09 00:24:59 -0400

    create main stylesheet

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./app";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/main.scss';
 
 ReactDOM.render(<App />, document.querySelector("#root"));
