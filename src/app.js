12eeda86e8f15d76f1ae58010af537b23b498d29
Ryan Rasmussen <ryan.rasmussen@annelewisllc.com>
2020-01-26 17:43:26 -0700

    switch date initialization to use moment

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ClockPage from "./components/clock/clockPage";
 import TextTransform from "./components/text/textTransform";
+import moment from 'moment-timezone';
 
 export default class App extends Component {
     render() {
@@ -9,7 +10,7 @@ export default class App extends Component {
             <Router>
                 <Switch>
                     <Route path="/text" component={TextTransform} />             
-                    <Route exact path="/"  component={() => <ClockPage time={new Date()} /> } />
+                    <Route exact path="/"  component={() => <ClockPage time={moment().format()} /> } />
                 </Switch>
             </Router>
         );
