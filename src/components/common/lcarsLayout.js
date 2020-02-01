db5cef5ffe7b93b4059e81e96c0b4332717145e0
Ryan Rasmussen <ryan.rasmussen@annelewisllc.com>
2020-02-01 17:26:29 -0500

    toggle menu on click

--- src/components/common/lcarsLayout.js
+++ src/components/common/lcarsLayout.js
@@ -2,13 +2,33 @@ import React, { Component } from "react";
 import Menu from "./menu";
 
 export default class Layout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpen: false,
+        }
+
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    toggleMenu() {
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        });
+    }
+    
     render() {
+        let menuText = this.state.menuOpen ? "Close" : "Menu";
+
         return (
             <div className="app-container-layout">
                 <div id="header" className="lcars-row header">
                     <div className="lcars-elbow left-bottom lcars-ruest-bg"></div>
                     <div className="lcars-bar horizontal menu">
-                        <Menu />
+                        <div 
+                            className="menu-label lcars-title right"
+                            onClick={this.toggleMenu}>{menuText}</div>
+                        <Menu menuOpen={this.state.menuOpen} />
                     </div>
                     <div className="lcars-bar horizontal right-end decorated"></div>
                 </div>
--- src/components/common/menu.js
+++ src/components/common/menu.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import Navigation from "./navigation";
 
 export default class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpen: this.props.menuOpen,
+        }
+    }
     render() {
+        let showMenu = this.props.menuOpen ? "show" : "hidden";
         return (
-            <div className="menu-container">
-                <div className="lcars-row">
+            <div className={`menu-container ${showMenu}`}>
+                <div id="top-bar" className="lcars-row">
                     <div className="lcars-bar horizontal left-end decorated lcars-dodger-blue-bg"></div>
                     <div className="lcars-bar horizontal lcars-dodger-blue-bg"></div>
                     <div className="lcars-elbow right-bottom lcars-dodger-blue-bg"></div>
