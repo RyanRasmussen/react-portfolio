a2a1fc80f347d8b4cec6db5b0e1d412656821abf
Ryan Rasmussen <ryan.rasmussen@annelewisllc.com>
2020-03-01 18:27:15 -0700

    initial lorem ipsum components

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ClockPage from "./components/clock/clockPage";
 import TextTransform from "./components/text/textTransform";
+import LoremIpsumGen from "./components/lorem/loremIpsum";
 import moment from 'moment-timezone';
 
 export default class App extends Component {
@@ -9,7 +10,8 @@ export default class App extends Component {
         return (
             <Router>
                 <Switch>
-                    <Route path="/text" component={TextTransform} />             
+                    <Route path="/text" component={TextTransform} />
+                    <Route path="/lorem" component={LoremIpsumGen} />           
                     <Route exact path="/"  component={() => <ClockPage time={moment().format()} /> } />
                 </Switch>
             </Router>
---
+++ src/components/lorem/loremIpsum.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Layout from '../common/lcarsLayout';
+import Button from '../common/actionButton';
+import {
+    Container,
+  } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+export default class LoremIpsumGen extends Component {
+    constructor(props) {
+    }
+}
